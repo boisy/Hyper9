67359c7a614409138b44c8d4a76a229817848389
Boisy Pitre <bpitre@apple.com>
2025-04-05 09:25:28 -0400

    Fixed issue with @StateObject being used outside of a view

--- Hyper9/SimDocument.swift
+++ Hyper9/SimDocument.swift
@@ -4,7 +4,7 @@ import UniformTypeIdentifiers
 // Define your document type.
 struct SimDocument: FileDocument {
     // The document’s content; adjust as needed.
-    @StateObject var disassembler = Turbo9ViewModel()
+    var disassembler = Turbo9ViewModel()
     var text: String = "Hello, SwiftUI Document App!"
 
     // Specify the content types that your document supports.
